3f9e8a3fad4df1a1b713d5c9c63a10273420ba0f
James Kristoff <james.kristoff@mathworks.com>
2018-02-16 15:10:11 -0600

    Added work history outline. updated js.

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,7 +1,7 @@
 var q = $(".btn[data-toggle='collapse']");
+var re = /top\.svg$/;
 q.each(function(index) {
     $(this).on("click", function() {
-      var re = /top\.svg$/;
       if($(this).children("img").length > 0) {
         var src = $(this).children("img").attr("src");
         match = src.match(re);
@@ -14,3 +14,12 @@ q.each(function(index) {
       }
     });
 });
+
+// var src = $(this).children("img").attr("src");
+// var shown = $($(this).attr("data-target")).attr("class").match("show");
+// if(shown) {
+//   $(this).children("img").attr("src", src.replace(/top\.svg$/, "bottom.svg"));
+// }
+// else {
+//   $(this).children("img").attr("src", src.replace(/bottom\.svg$/, "top.svg"));
+// }
